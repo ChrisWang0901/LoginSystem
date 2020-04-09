e9d1dbfa2cbe3f723e62e6cdf94b804a05ea20bc
ChrisWang0901 <58105627+ChrisWang0901@users.noreply.github.com>
2020-04-08 23:41:33 -0700

    fix style

--- routes/login.js
+++ routes/login.js
@@ -21,17 +21,5 @@ router.post("/", async (req, res) => {
     console.error(error);
   }
 });
-// router.get(
-//   "/auth/google",
-//   passport.authenticate("google", { scope: ["profile"] })
-// );
 
-// router.get(
-//   "/auth/google/callback",
-//   passport.authenticate("google", { failureRedirect: "/" }),
-//   function (req, res) {
-//     // Successful authentication, redirect home.
-//     res.redirect("/home");
-//   }
-// );
 module.exports = router;
